c88d723af2e371666106e18eef59621cd6df3257
David Alen <davidalen51@gmail.com>
2020-03-11 00:36:01 -0300

    added Builder Pattern

---
+++ Builder/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace Builder
+{
+	interface Input
+	{
+		string ReadLines();
+	}
+
+	interface IBuilder
+	{
+		Input Build(string args);
+	}
+
+	class Reader : IBuilder
+	{
+		public Input Build(string args)
+		{
+			if (args.Equals("txt"))
+			{
+				return new TXTReader();
+			}
+			return new CSVReader();
+		}
+	}
+
+	class CSVReader : Input
+	{
+		public string ReadLines()
+		{
+			return "CSV!";
+		}
+	}
+
+	class TXTReader : Input
+	{
+		public string ReadLines()
+		{
+			return "TXT!";
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string arguments = "txt";
+			Reader reader = new Reader();
+			Input input = reader.Build(arguments);
+
+			Console.WriteLine(input.ReadLines());
+		}
+	}
+}
