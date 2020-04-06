cbe8b828a105d6dcfb4376a069e88e05516d5f3d
David Alen <davidalen51@gmail.com>
2020-04-06 06:20:30 -0300

    Added Command Pattern

---
+++ Command/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Command
+{
+	class Firearm
+	{
+		public int bulletsCount;
+		public int bulletsMax;
+		public bool canFire
+		{
+			get { return bulletsCount > 0; }
+
+		}
+
+		public Firearm(int bullets)
+		{
+			bulletsCount = bullets;
+			bulletsMax = bullets;
+		}
+
+		public void Reload()
+		{
+			bulletsCount = bulletsMax;
+			Console.WriteLine("Reloaded!");
+		}
+		public void Fire()
+		{
+			if (bulletsCount > 0)
+			{
+				bulletsCount--;
+				Console.WriteLine("Fired!");
+			}
+			else
+			{
+				Console.WriteLine("Could not fire 'cause there is no bullets!");
+			}
+		}
+	}
+
+	class Command
+	{
+		public void Execute(Action action)
+		{
+			action.Invoke();
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Firearm firearm = new Firearm(9);
+			Command command = new Command();
+
+			for (int i = 0; i < 10; i++)
+			{
+				command.Execute(firearm.Fire);
+			}
+
+			command.Execute(firearm.Reload);
+		}
+	}
+}
