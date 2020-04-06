ca4ae4bf4788823cbb317bb3ba9e93ada1b340de
David Alen <davidalen51@gmail.com>
2020-04-06 07:16:46 -0300

    Added Interpreter Pattern

---
+++ Interpreter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Interpreter
+{
+	interface IInterpreter
+	{
+		string Interpret();
+	}
+
+	class Binary : IInterpreter
+	{
+		public string Context;
+
+		public Binary(string context) => Context = context;
+
+		public string Interpret()
+		{
+			int i = 0, sum = 0;
+			foreach (char c in Context.ToCharArray())
+			{
+				if (c == '1')
+				{
+					sum += (int)Math.Pow(2, i);
+				}
+
+				i++;
+			};
+			return sum.ToString();
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Binary bin = new Binary("11100101");
+			string interpreted = bin.Interpret();
+
+			Console.WriteLine(interpreted);
+		}
+	}
+}
