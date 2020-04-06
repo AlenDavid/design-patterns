2521a3fddb4caca8cffb96a0d8374d9d933929d8
David Alen <davidalen51@gmail.com>
2020-04-06 00:06:23 -0300

    Facade Pattern

---
+++ Facade/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Facade
+{
+	class CharacterJob
+	{
+		public string JobName;
+
+		public CharacterJob(string jobName) => JobName = jobName;
+
+		public void Work() => Console.WriteLine("Working as " + JobName);
+	}
+
+	class CharacterClass
+	{
+		public string ClassName;
+
+		public CharacterClass(string className) => ClassName = className;
+
+		public void Attack() => Console.WriteLine("Attacking as " + ClassName);
+	}
+
+	class Character
+	{
+		public CharacterJob Job;
+		public CharacterClass Class;
+
+		public Character(string jobName, string className)
+		{
+			Job = new CharacterJob(jobName);
+			Class = new CharacterClass(className);
+		}
+
+		public void Work() => Job.Work();
+		public void Attack() => Class.Attack();
+	}
+
+	class Program
+	{
+		public static void Main(String[] args)
+		{
+			Character character = new Character("Smith", "Paladin");
+
+			character.Work();
+			character.Attack();
+		}
+	}
+}
