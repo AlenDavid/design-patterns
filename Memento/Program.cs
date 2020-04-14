50045e6667c7354261201392015a257c69ee9ccf
David Alen <davidalen51@gmail.com>
2020-04-14 02:10:05 -0300

    Add Memento

---
+++ Memento/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Memento
+{
+
+  class Notation
+  {
+    public string Text = "Default";
+
+    public void SetMemento(Memento memento) => Text = memento.Notation.Text;
+    public Memento CreateMemento() => new Memento((Notation)this.MemberwiseClone());
+  }
+
+  class Memento
+  {
+    public Notation Notation;
+
+    public Memento(Notation notation) => Notation = notation;
+
+    public void Keep(Notation notation) => Notation = notation;
+    public Notation Restore() => Notation;
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Notation notation = new Notation();
+      Memento m1 = notation.CreateMemento();
+
+      notation.Text = "Rychi";
+      Memento m2 = notation.CreateMemento();
+
+      notation.Text = "David";
+      Memento m3 = notation.CreateMemento();
+
+      Console.WriteLine(notation.Text);
+      notation.SetMemento(m2);
+      Console.WriteLine(notation.Text);
+    }
+  }
